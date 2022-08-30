e5236048b743e05973ed0966fcb583a590d61613
Luciano Ramo - Gazai <luciano.ramo76@gmail.com>
2022-08-30 04:36:43 -0300

    added api services and layout

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,35 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import { getWeatherData } from "../services";
+import { Main } from "../components";
 
 
 export default function Home() {
-  return (
-    <div>
-        Start
-    </div>
-  )
+
+ /*        const [coords, setcoords] = useState({ "latitude": -20.2663, "longitude": -40.3165 });
+        const [weatherData, setWeatherData] = useState({});
+
+        const getCoords = useCallback(() => {
+                navigator.geolocation.getCurrentPosition(function (position) {
+                        return setcoords(position.coords);
+                });
+                console.log(coords);
+        }, []);
+
+        useEffect(() => {
+                getCoords();
+                getWeatherData(coords.latitude, coords.longitude).then(data => {
+                        setWeatherData(data);
+                }).catch(err => {
+                        console.log(err);
+                });
+        }, [getCoords]); */
+
+
+        return (
+                <div>
+                        <Main />
+                        
+                </div>
+        )
 }
+

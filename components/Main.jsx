477dcab784cc44e153c2b831b5dc626c1390fcc0
Luciano Ramo - Gazai <luciano.ramo76@gmail.com>
2022-08-30 18:39:07 -0300

    updated interface and getData from api

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,70 +1,97 @@
 import React from "react";
 import Image from "next/image";
+import moment from "moment";
 
 const Main = ({ props }) => {
     return (
         <div className="container mx-auto px-10 mb-8">
             <div className="grid grid-cols-1 lg:grid-cols-12 gap-8">
                 <div className="lg:col-span-8 col-span-1 lg:col-start-3">
-                    <div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
-                        <div className="flex justify-between items-center px-6 py-4">
-                            <h4 className="text-sm">12 Out, 2022</h4>
+                    <div className="bg-white shadow-lg rounded-lg p-4 lg:p-8 pb-12 mb-8">
+                        <div className="flex justify-between items-center ">
+                            <h4 className="text-sm">
+                                {moment(Date.now()).format("DD MMM, YYYY")}
+                            </h4>
 
                             <div className="flex items-center">
-                                <h4 className="text-sm">4:35am</h4>
+                                <h4 className="text-sm">
+                                    {moment(Date.now()).format("LT")}
+                                </h4>
                             </div>
                         </div>
-                        <div className="flex-v text-center justify-center items-center px-6 py-4">
+                        <div className="flex-v text-center justify-center items-center px-6 py-4 mb-8">
                             <div>
-                                <h4 className="text-sm block">Cidade</h4>
+                                <h1 className="text-4xl text-indigo-400 font-light block">
+                                    {props.name}
+                                </h1>
                             </div>
                             <div>
-                                <h4 className="text-sm block">Ensolarado</h4>
+                                <h2 className="text-lg text-gray-500 font-light block">
+                                    {props.weather[0].description}
+                                </h2>
                             </div>
                         </div>
-                        <div className="w-full flex items-center justify-center">
+                        <div className="w-full flex items-center justify-center mb-8">
                             <Image
                                 src="/../public/sol.png"
                                 alt=""
-                                width="200px"
-                                height="200px"
+                                width="150px"
+                                height="150px"
                             />
                         </div>
                         <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-                            <div className="flex justify-center items-start text-center ">
-                                <h1 className="text-8xl text-gray-700">30</h1>
-                                <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
-                                    <div className="flex justify-center w-full items-center text-center ">
-                                        <p className="text-sm text-gray-700">
-                                            25
-                                        </p>
-                                    </div>
-                                    <div className="flex justify-center w-full items-center text-center ">
-                                        <p className="text-sm text-gray-700">
-                                            26
-                                        </p>
-                                    </div>
+                            <div className="flex-v justify-center items-center text-center ">
+                                <div className="text-[12rem] leading-tight font-extralight text-gray-500 tracking-tighter">
+                                    {Math.round(props.main.temp)}
+                                    <span className="text-2xl text-start">
+                                        &#8451;
+                                    </span>
+                                </div>
+                                <div className="">
+                                    <h4 className="text-sm text-gray-700">
+                                        Sensação Térmica:{" "}
+                                        <span className="text-2xl">
+                                            {Math.round(props.main.feels_like)}
+                                            &#8451;
+                                        </span>
+                                    </h4>
                                 </div>
                             </div>
-                            <div className="w-full text-center">
-                                <table className="table-fixed">
+                            <div className="flex text-center justify-center items-center p-4">
+                                <table className="border-collapse">
                                     <tbody>
                                         <tr>
-                                            <td className="text-center">
-                                                sunrise
+                                            <td className="px-4 py-4">
+                                                Umidade:
                                             </td>
-                                            <td className="text-center">
-                                                tabela
+                                            <td className="text-2xl text-gray-600 px-4 py-4">
+                                                {props.main.humidity}%
                                             </td>
                                         </tr>
                                         <tr>
-                                            <td className="text-center">
-                                                tabela
+                                            <td className="px-4 py-4">
+                                                Vento:
+                                            </td>
+                                            <td className="text-2xl text-gray-600 px-4 py-4">
+                                                {props.wind.speed}km/h
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td className="px-4 py-4">
+                                                Dir. Vento:
+                                            </td>
+                                            <td className="text-2xl text-gray-600 px-4 py-4">
+                                                {props.wind.deg}°
                                             </td>
                                         </tr>
                                     </tbody>
                                 </table>
                             </div>
+                        </div>
+                        <div className="flex justify-center items-center text-center my-8 ">
+                            <span className="transition duration-500 ease transform hover:bg-slate-600 hover:text-white inline-block  text-lg font-medium rounded-full border-2 text-slate-800 px-8 py-3 cursor-pointer">
+                                Atualizar
+                            </span>
                         </div>
                     </div>
                 </div>
--- pages/index.js
+++ pages/index.js
@@ -2,33 +2,31 @@ import React, { useState, useEffect, useCallback } from 'react';
 import { getWeatherData } from "../services";
 import { Main } from "../components";
 
-
 export default function Home() {
 
- /*        const [coords, setcoords] = useState({ "latitude": -20.2663, "longitude": -40.3165 });
+        const [coords, setcoords] = useState([]);
         const [weatherData, setWeatherData] = useState({});
 
-        const getCoords = useCallback(() => {
-                navigator.geolocation.getCurrentPosition(function (position) {
-                        return setcoords(position.coords);
-                });
-                console.log(coords);
-        }, []);
 
         useEffect(() => {
-                getCoords();
-                getWeatherData(coords.latitude, coords.longitude).then(data => {
-                        setWeatherData(data);
-                }).catch(err => {
-                        console.log(err);
-                });
-        }, [getCoords]); */
 
+                navigator.geolocation.getCurrentPosition(function (position) {
+                        
+                        getWeatherData(position.coords.latitude, position.coords.longitude).then(data => {
+                                setWeatherData(data);
+                                console.log(data);
+                        }).catch(err => {
+                                console.log(err);
+                        });
+
+                });
+
+
+        }, []);
 
         return (
                 <div>
-                        <Main />
-                        
+                        {<Main props={weatherData}/>}
                 </div>
         )
 }
